9cbb938dcb14c1f2c4faa5c6701979931d4cc442
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-27 01:31:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,10 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 
-function greet("HH:MM")
+function greet(string) {
+  const hour = parseInt(string, 10);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+    return "Good Afternoon"
+}
 /* Write your implementation of displayMessage() */
